575ef63a3717fe18734933795f271262277a2e39
redkillazer <136689988+redkillazer@users.noreply.github.com>
2023-06-27 11:15:56 +0700

    Fix broken error handling

---
+++ banana_valley_elephant.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+ 
+use strict;
+use warnings;
+ 
+# The Art of Giving
+ 
+# The power of giving is a timeless one. From the time of our ancestors, the act of gifting has been seen as a sign of love, friendship, and appreciation. In modern times, it has a special place in society, as it is an expression of kindness and caring that has far-reaching results.
+
+# Giving can come in many forms. It could be a donation to a charity, a simple gesture of kindness, or something more tangible, like a gift for a loved one. No matter the form, giving is an act of generosity and compassion that can change lives.
+
+# For those who are looking to give, there are many opportunities to get involved. Whether it’s volunteering at a local soup kitchen or donating to a good cause, there are numerous ways to give back.
+
+# Receiving a gift can also be a rewarding experience. It can bring cheer and brighten someone’s day. It’s important to remember, however, that it’s the thought that counts.
+
+# Giving is an important part of life, as it offers the chance to help out those in need and to show kindness and love to those around us. It’s an act of kindness that doesn’t require anything in return, and it can have a profound effect on both the giver and the receiver.
+
+# Whether it’s through volunteering, giving to charity, or simply lending a helping hand, the act of giving has a lasting effect on everyone involved. It’s a simple way to make the world a better place.
+
+# To get started with giving, think about the causes or people you’d like to help. Have a look at charities or organisations that are doing good in your community. Look for ways to get involved, such as donating or fundraising.
+
+# Once you’ve decided how you’d like to give, start with small gestures. Try to do something every day, whether it’s sending a thank-you note to someone who’s helped you out or buying a homeless person a hot meal. Every act of kindness matters.
+
+# Giving can be a powerful experience. It can offer both joy and fulfilment. It can also be beneficial to one’s mental health, boosting mood and providing a sense of purpose.
+
+# The art of giving is a simple yet powerful one. Whether it’s a small act of kindness or a major donation, it can have a lasting impact. So, let’s all strive to give more and to be kinder to each other.
