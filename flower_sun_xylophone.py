d90daa9e4cfc15d58ee4d773ddf3c5ffcacba372
redkillazer <136689988+redkillazer@users.noreply.github.com>
2023-06-28 07:48:19 +0700

    Simplify code logic

---
+++ flower_sun_xylophone.py
@@ -0,0 +1,413 @@
+import time
+
+#1
+def give_gift(gift):
+    '''A function to give a gift to someone.'''
+    print('You have given a ' + gift + ' as a gift.')
+
+#2
+def send_card(recipient):
+    '''A function to send a card to someone.'''
+    print('You have sent a card to ' + recipient)
+
+#3
+def plan_outing(people):
+    '''A function to plan an outing for a group of people.'''
+    print('You have planned an outing for ' + people)
+
+#4
+def make_meal(dish):
+    '''A function to make a meal for someone.'''
+    print('You have made a ' + dish + ' for your loved one.')
+
+#5
+def write_letter(recipient):
+    '''A function to write a letter to someone.'''
+    print('You have written a letter to ' + recipient)
+
+#6
+def buy_gift(gift):
+    '''A function to buy a gift for someone.'''
+    print('You have bought a ' + gift + ' for your love one.')
+
+#7
+def wrap_gift(gift):
+    '''A function to wrap a gift for someone.'''
+    print('You have wrapped a ' + gift + ' for your love one.')
+
+#8
+def volunteer_time(cause):
+    '''A function to volunteer your time for a cause.'''
+    print('You have volunteered your time to ' + cause)
+
+#9
+def donate_goods(goods):
+    '''A function to donate goods to charity.'''
+    print('You have donated ' + goods + ' to charity.')
+
+#10
+def give_compliment(person):
+    '''A function to give a compliment to someone.'''
+    print('You have given a compliment to ' + person)
+
+#11
+def give_hug(person):
+    '''A function to give a hug to someone.'''
+    print('You have given a hug to ' + person)
+
+#12
+def send_flowers(recipient):
+    '''A function to send flowers to someone.'''
+    print('You have sent flowers to ' + recipient)
+
+#13
+def give_encouragement(person):
+    '''A function to give encouragement to someone.'''
+    print('You have encouraged ' + person)
+
+#14
+def give_time(person):
+    '''A function to give your time to someone.'''
+    print('You have given your time to ' + person)
+
+#15
+def pick_up_litter(area):
+    '''A function to pick up litter in a given area.'''
+    print('You have picked up litter in ' + area)
+
+#16
+def clean_up_neighborhood(neighborhood):
+    '''A function to clean up a neighborhood.'''
+    print('You have cleaned up the ' + neighborhood + ' neighborhood.')
+
+#17
+def help_neighbor(neighbor):
+    '''A function to help your neighbor.'''
+    print('You have helped your neighbor ' + neighbor + '.')
+
+#18
+def offer_assistance(person):
+    '''A function to offer assistance to someone.'''
+    print('You have offered assistance to ' + person)
+
+#19
+def offer_listening_ear(person):
+    '''A fucntion to offer a listening ear to someone.'''
+    print('You have offered a listening ear to ' + person)
+
+#20
+def clean_up_beach(beach):
+    '''A function to clean up a beach.'''
+    print('You have cleaned up the ' + beach + ' beach.')
+
+#21
+def give_support(person):
+    '''A function to give support to someone.'''
+    print('You have given support to ' + person)
+
+#22
+def do_favors(person):
+    '''A function to do favors for someone.'''
+    print('You have done favors for ' + person)
+
+#23
+def give_advice(person):
+    '''A function to give advice to someone.'''
+    print('You have given advice to ' + person)
+
+#24
+def plant_trees(area):
+    '''A function to plant trees in a given area.'''
+    print('You have planted trees in ' + area)
+
+#25
+def donate_clothes(organization):
+    '''A function to donate clothes to an organization.'''
+    print('You have donated clothes to ' + organization)
+
+#26
+def give_feedback(person):
+    '''A function to give feedback to someone.'''
+    print('You have given feedback to ' + person)
+
+#27
+def be_patient(person):
+    '''A function to be patient with someone.'''
+    print('You have been patient with ' + person)
+
+#28
+def do_chores(person):
+    '''A function to do chores for someone.'''
+    print('You have done chores for ' + person)
+
+#29
+def give_encouragement(person):
+    '''A function to give encouragement to someone.'''
+    print('You have given encouragement to ' + person)
+
+#30
+def pick_up_trash(area):
+    '''A function to pick up trash in a given area.'''
+    print('You have picked up trash in ' + area)
+
+#31
+def make_handmade_gift(recipient):
+    '''A function to make a handmade gift for someone.'''
+    print('You have made a handmade gift for ' + recipient)
+
+#32
+def donate_blood(organization):
+    '''A function to donate blood to an organization.'''
+    print('You have donated blood to ' + organization)
+
+#33
+def offer_hope(person):
+    '''A function to offer hope to someone.'''
+    print('You have offered hope to ' + person)
+
+#34
+def tell_story(person):
+    '''A function to tell a story to someone.'''
+    print('You have told a story to ' + person)
+
+#35
+def give_help(person):
+    '''A function to give help to someone.'''
+    print('You have given help to ' + person)
+
+#36
+def help_the_homeless(homeless_person):
+    '''A function to help a homeless person.'''
+    print('You have helped a homeless person ' + homeless_person + '.')
+
+#37
+def feed_hungry(hungry_person):
+    '''A function to feed a hungry person.'''
+    print('You have fed a hungry person ' + hungry_person + '.')
+
+#38
+def give_compliment(person):
+    '''A function to give a compliment to someone.'''
+    print('You have given a compliment to ' + person)
+
+#39
+def give_support_group(group):
+    '''A function to give support to a group.'''
+    print('You have given support to ' + group)
+
+#40
+def donate_money(organization):
+    '''A function to donate money to an organization.'''
+    print('You have donated money to ' + organization)
+
+#41
+def provide_meal(person):
+    '''A function to provide a meal to someone.'''
+    print('You have provided a meal to ' + person)
+
+#42
+def spread_positivity(people):
+    '''A function to spread positivity amongst people.'''
+    print('You have spread positivity amongst ' + people)
+
+#43
+def give_time(person):
+    '''A function to give your time to someone.'''
+    print('You have given your time to ' + person)
+
+#44
+def give_handmade_card(recipient):
+    '''A function to give a handmade card to someone.'''
+    print('You have given a handmade card to ' + recipient)
+
+#45
+def pay_it_forward(person):
+    '''A function to pay it forward to someone.'''
+    print('You have paid it forward to ' + person)
+
+#46
+def offer_kind_words(person):
+    '''A function to offer kind words to someone.'''
+    print('You have offered kind words to ' + person)
+
+#47
+def thank_people(people):
+    '''A function to thank people.'''
+    print('You have thanked ' + people)
+
+#48
+def offer_comfort(person):
+    '''A function to offer comfort to someone.'''
+    print('You have offered comfort to ' + person)
+
+#49
+def help_animal_shelter(shelter):
+    '''A function to help an animal shelter.'''
+    print('You have helped an animal shelter ' + shelter + '.')
+
+#50
+def give_dignity(person):
+    '''A function to give dignity to someone.'''
+    print('You have given dignity to ' + person)
+
+#51
+def give_smile(person):
+    '''A function to give a smile to someone.'''
+    print('You have given a smile to ' + person)
+
+#52
+def pray_for_someone(person):
+    '''A function to pray for someone.'''
+    print('You have prayed for ' + person)
+
+#53
+def donate_books(organization):
+    '''A function to donate books to an organization.'''
+    print('You have donated books to ' + organization)
+
+#54
+def help_clean_up_park(park):
+    '''A function to help clean up a park.'''
+    print('You have helped clean up the ' + park + ' park.')
+
+#55
+def give_love(person):
+    '''A function to give love to someone.'''
+    print('You have given love to ' + person)
+
+#56
+def give_respect(person):
+    '''A function to give respect to someone.'''
+    print('You have given respect to ' + person)
+
+#57
+def put_positive_sign(area):
+    '''A function to put a positive sign in a given area.'''
+    print('You have put a positive sign in ' + area)
+
+#58
+def offer_warm_clothes(person):
+    '''A function to offer warm clothes to someone.'''
+    print('You have offered warm clothes to ' + person)
+
+#59
+def donate_food(organization):
+    '''A function to donate food to an organization.'''
+    print('You have donated food to ' + organization)
+
+#60
+def make_gift_basket(recipient):
+    '''A function to make a gift basket for someone.'''
+    print('You have made a gift basket for ' + recipient)
+
+#61
+def offer_listening_ear(person):
+    '''A fucntion to offer a listening ear to someone.'''
+    print('You have offered a listening ear to ' + person)
+
+#62
+def help_collect_items(organization):
+    '''A function to help collect items for an organization.'''
+    print('You have helped collect items for ' + organization)
+
+#63
+def give_accolade(person):
+    '''A function to give an accolade to someone.'''
+    print('You have given an accolade to ' + person)
+
+#64
+def give_gift_card(recipient):
+    '''A function to give a gift card to someone.'''
+    print('You have given a gift card to ' + recipient)
+
+#65
+def help_needy_family(family):
+    '''A function to help a needy family.'''
+    print('You have helped a needy family ' + family + '.')
+
+#66
+def pick_up_litter(area):
+    '''A function to pick up litter in a given area.'''
+    print('You have picked up litter in ' + area)
+
+#67
+def volunteer_your_time(cause):
+    '''A function to volunteer your time for a cause.'''
+    print('You have volunteered your time to ' + cause)
+
+#68
+def give_donation(cause):
+    '''A function to give a donation to a cause.'''
+    print('You have given a donation to ' + cause)
+
+#69
+def donate_blankets(organization):
+    '''A function to donate blankets to an organization.'''
+    print('You have donated blankets to ' + organization)
+
+#70
+def give_empowerment(person):
+    '''A function to give empowerment to someone.'''
+    print('You have given empowerment to ' + person)
+
+#71
+def give_lovely_surprise(recipient):
+    '''A fucntion to give a lovely surprise to someone.'''
+    print('You have given a lovely surprise to ' + recipient)
+
+#72
+def clean_up_river(river):
+    '''A function to clean up a river.'''
+    print('You have cleaned up the ' + river + ' river.')
+
+#73
+def do_random_act_of_kindness(person):
+    '''A function to do a random act of kindness for someone.'''
+    print('You have done a random act of kindness for ' + person)
+
+#74
+def give_insight(person):
+    '''A function to give insight to someone.'''
+    print('You have given insight to ' + person)
+
+#75
+def give_warm_hug(person):
+    '''A function to give a warm hug to someone.'''
+    print('You have given a warm hug to ' + person)
+
+#76
+def give_greeting_card(recipient):
+    '''A function to give a greeting card to someone.'''
+    print('You have given a greeting card to ' + recipient)
+
+#77
+def offer_sympathy(person):
+    '''A function to offer sympathy to someone.'''
+    print('You have offered sympathy to ' + person)
+
+#78
+def give_toy(child):
+    '''A function to give a toy to a child.'''
+    print('You have given a toy to ' + child)
+
+#79
+def donate_toys(organization):
+    '''A function to donate toys to an organization.'''
+    print('You have donated toys to ' + organization)
+
+#80
+def give_care_package(recipient):
+    '''A function to give a care package to someone.'''
+    print('You have given a care package to ' + recipient)
+
+#81
+def be_positive(person):
+    '''A fucntion to be positive with someone.'''
+    print('You have been positive with ' + person)
+
+#82
+def give_kind_remark(person):
+    '''A function to give a kind remark to someone.'''
+    print('You have given a kind remark to ' + person)
+
+#83
