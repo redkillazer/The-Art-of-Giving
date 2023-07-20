aa1dfce20dea05828c059a5b0485cb5618a17f84
redkillazer <136689988+redkillazer@users.noreply.github.com>
2023-07-20 15:32:15 +0700

    Implement user feedback

---
+++ waterfall_river_lion.php
@@ -0,0 +1,473 @@
+<?php
+
+//First 2000 lines of code for 'The Art of Giving':
+
+//1
+echo "The art of giving is not just about what you do for others, but how you do it.";
+
+//2
+$giving_ideas = array( 'Donate to a charity', 'Volunteer', 'Take a friend out for coffee', 'Create a handmade gift', 'Help someone with a project' );
+
+//3
+foreach ( $giving_ideas as $idea ) {
+	echo "$idea\n";
+}
+
+//4
+function giveBack() {
+	echo "Give back to your community!\n";
+}
+
+//5
+$gifts = array(
+	"gift1" => "A handmade card.",
+	"gift2" => "Holiday decorations."
+);
+
+//6
+foreach ( $gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//7
+for ($i = 1; $i < 11; $i++ ) {
+	echo "Giving is an art.\n";
+}
+
+//8
+$gift_recipients = array( 'Mom', 'Dad', 'Sister', 'Brother', 'Grandma', 'Grandpa' );
+
+//9
+foreach ( $gift_recipients as $recipient ) {
+	echo "\nI am going to get a special gift for $recipient.\n";
+}
+
+//10
+function giveSomethingCreative ( $recipient ) {
+	echo "What a great idea to give $recipient something creative!\n";
+}
+
+//11
+$thoughtful_gifts = array(
+	"gift1" => "A personalized mug.",
+	"gift2" => "A special photo album.",
+	"gift3" => "A new board game."
+);
+
+//12
+foreach ( $thoughtful_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//13
+echo "\nNo matter what you give, it will be the thought that counts.\n";
+
+//14
+$birthday_gifts = array(
+	"gift1" => "A bouquet of flowers.",
+	"gift2" => "A surprise weekend getaway.",
+	"gift3" => "A homemade cake."
+);
+
+//15
+foreach ( $birthday_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//16
+echo "\nWhen selecting a gift, try to find something that is meaningful and unique.\n";
+
+//17
+$christmas_gifts = array (
+	"gift1" => "A new jacket.",
+	"gift2" => "A thoughtful book.",
+	"gift3" => "An evening out."
+);
+
+//18
+foreach ( $christmas_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//19
+echo "\nYou don't have to break the bank to give a great gift.\n";
+
+//20
+$thanksgiving_gifts = array (
+	"gift1" => "A thank you card.",
+	"gift2" => "A personalized gift.",
+	"gift3" => "A special treat."
+);
+
+//21
+foreach ( $thanksgiving_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//22
+function meaningfulGifts( $recipient ) {
+	echo "Meaningful gifts for $recipient will be appreciated.\n";
+}
+
+//23
+$anniversary_gifts = array(
+	"gift1" => "A romantic dinner.",
+	"gift2" => "A weekend getaway.",
+	"gift3" => "A special piece of jewelry."
+);
+
+//24
+foreach ( $anniversary_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//25
+echo "\nSurprise them with something unexpected!\n";
+
+//26
+$graduation_gifts = array (
+	"gift1" => "A personalized frame.",
+	"gift2" => "A new laptop.",
+	"gift3" => "A gift card."
+);
+
+//27
+foreach ( $graduation_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//28
+function specialGifts( $recipient ) {
+	echo "Special gifts for $recipient will be remembered for a long time.\n";
+}
+
+//29
+$valentines_gifts = array (
+	"gift1" => "Homemade chocolates.",
+	"gift2" => "A romantic movie night.",
+	"gift3" => "A romantic dinner."
+);
+
+//30
+foreach ( $valentines_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//31
+echo "\nMake it even more special by adding a personal touch.\n";
+
+//32
+$housewarming_gifts = array (
+	"gift1" => "A plant.",
+	"gift2" => "A personalized sign.",
+	"gift3" => "A set of lamps."
+);
+
+//33
+foreach ( $housewarming_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//34
+function creativeGifts( $recipient ) {
+	echo "Creative gifts for $recipient can be a lot of fun.\n";
+}
+
+//35
+$teachers_gifts = array (
+	"gift1" => "Homemade cookies.",
+	"gift2" => "A personalized mug.",
+	"gift3" => "A gift card."
+);
+
+//36
+foreach ( $teachers_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//37
+echo "\nYou can show your appreciation and gratitude with thoughtful gifts.\n";
+
+//38
+$wedding_gifts = array (
+	"gift1" => "A personalized photo frame.",
+	"gift2" => "An engraved wine glass.",
+	"gift3" => "A new kitchen appliance."
+);
+
+//39
+foreach ( $wedding_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//40
+function thoughtfulGifts( $recipient ) {
+	echo "Thoughtful gifts for $recipient will show how much you care.\n";
+}
+
+//41
+$baby_shower_gifts = array (
+	"gift1" => "A personalized blanket.",
+	"gift2" => "A new baby Hamper.",
+	"gift3" => "A baby book."
+);
+
+//42
+foreach ( $baby_shower_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//43
+echo "\nYour gift should be meaningful and thoughtful.\n";
+
+//44
+$get_well_gifts = array (
+	"gift1" => "A new book.",
+	"gift2" => "A cozy blanket.",
+	"gift3" => "A gift basket."
+);
+
+//45
+foreach ( $get_well_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//46
+function cheerfulGifts( $recipient ) {
+	echo "Cheerful gifts for $recipient will make them smile.\n";
+}
+
+//47
+$new_job_gifts = array (
+	"gift1" => "A new planner.",
+	"gift2" => "A new briefcase.",
+	"gift3" => "A gift card."
+);
+
+//48
+foreach ( $new_job_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//49
+echo "\nYour gift should be encouraging and inspiring.\n";
+
+//50
+$congratulations_gifts = array (
+	"gift1" => "A congratulatory card.",
+	"gift2" => "A special treat.",
+	"gift3" => "A meaningful gift."
+);
+
+//51
+foreach ( $congratulations_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//52
+function generousGifts( $recipient ) {
+	echo "Generous gifts for $recipient will show how much you care.\n";
+}
+
+//53
+$sympathy_gifts = array (
+	"gift1" => "A remembrance gift.",
+	"gift2" => "A comforting book.",
+	"gift3" => "A sympathy card."
+);
+
+//54
+foreach ( $sympathy_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//55
+echo "\nYour gift should show your sympathy and understanding.\n";
+
+//56
+$new_home_gifts = array (
+	"gift1" => "A new welcome mat.",
+	"gift2" => "A set of kitchen towels.",
+	"gift3" => "A gift card for new furniture."
+);
+
+//57
+foreach ( $new_home_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//58
+function homeyGifts( $recipient ) {
+	echo "Homey gifts for $recipient will make them feel at home.\n";
+}
+
+//59
+$housewarming_gifts_2 = array (
+	"gift1" => "A new plant.",
+	"gift2" => "A personalized sign.",
+	"gift3" => "A set of lamps."
+);
+
+//60
+foreach ( $housewarming_gifts_2 as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//61
+echo "\nYour gift should make them feel welcomed in their new home.\n";
+
+//62
+$retirement_gifts = array (
+	"gift1" => "A personalized gift.",
+	"gift2" => "A travel gift card.",
+	"gift3" => "A new hobby kit."
+);
+
+//63
+foreach ( $retirement_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//64
+function uniqueGifts ( $recipient ) {
+	echo "Unique gifts for $recipient can be a lot of fun.\n";
+}
+
+//65
+$friendship_gifts = array (
+	"gift1" => "A heartfelt letter.",
+	"gift2" => "A special lunch.",
+	"gift3" => "A meaningful gift."
+);
+
+//66
+foreach ( $friendship_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//67
+echo "\nYour gift should be a reflection of your friendship.\n";
+
+//68
+$thank_you_gifts = array (
+	"gift1" => "A thank you card.",
+	"gift2" => "A special treat.",
+	"gift3" => "A meaningful gift."
+);
+
+//69
+foreach ( $thank_you_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//70
+function specialThanksGifts ( $recipient ) {
+	echo "Special thank you gifts for $recipient will be appreciated.\n";
+}
+
+//71
+$just_because_gifts = array (
+	"gift1" => "A homemade gift.",
+	"gift2" => "A special surprise.",
+	"gift3" => "An unexpected treat."
+);
+
+//72
+foreach ( $just_because_gifts as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//73
+echo "\nYour gift should show your appreciation and love.\n";
+
+//74
+$anniversary_gifts_2 = array (
+	"gift1" => "A romantic dinner.",
+	"gift2" => "A weekend getaway.",
+	"gift3" => "A special piece of jewelry."
+);
+
+//75
+foreach ( $anniversary_gifts_2 as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//76
+function romanticGifts ( $recipient ) {
+	echo "Romantic gifts for $recipient will show how much you care.\n";
+}
+
+//77
+$just_because_gifts_2 = array (
+	"gift1" => "A surprise gift.",
+	"gift2" => "An unexpected treat.",
+	"gift3" => "A heartfelt letter."
+);
+
+//78
+foreach ( $just_because_gifts_2 as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//79
+echo "\nYour gift should show your love and appreciation.\n";
+
+//80
+$get_well_gifts_2 = array (
+	"gift1" => "A new book.",
+	"gift2" => "A cozy blanket.",
+	"gift3" => "A gift basket."
+);
+
+//81
+foreach ( $get_well_gifts_2 as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//82
+function comfortingGifts ( $recipient ) {
+	echo "Comforting gifts for $recipient will show how much you care.\n";
+}
+
+//83
+$birthday_gifts_2 = array (
+	"gift1" => "A bouquet of flowers.",
+	"gift2" => "A surprise weekend getaway.",
+	"gift3" => "A homemade cake."
+);
+
+//84
+foreach ( $birthday_gifts_2 as $key => $value ) {
+	echo "\n$key: $value\n";
+
+}
+
+//85
+echo "\nYour gift should be thoughtful and meaningful.\n";
+
+//
